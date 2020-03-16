54247378ed7f9abe01dd2ef5606231aae4d1e917
noemiandor <noemi.andor@gmail.com>
2020-03-16 15:16:54 -0400

    fixed ENSEMBL error link for windows

--- R/aggregateSegmentExpression.R
+++ R/aggregateSegmentExpression.R
@@ -4,20 +4,22 @@ aggregateSegmentExpression <- function(epg, segments, mingps = 20, GRCh = 37) {
     cells = colnames(epg)
     
     ## Map gene IDs to genomic coordinates
+    ensembl = NULL
     if (GRCh == 36) {
-        ensembl = useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "may2009.archive.ensembl.org")
+        ensembl = try(useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "may2009.archive.ensembl.org"))
     } else if (GRCh == 37) {
         ensembl = try(useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "feb2014.archive.ensembl.org"), silent = T)
         if (class(ensembl) == "try-error") {
-            ensembl = useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "grch37.ensembl.org")
+            ensembl = try(useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "grch37.ensembl.org"))
         }
     } else if (GRCh == 38) {
         # ensembl = useEnsembl(biomart='ensembl',GRCh=NULL)
         ensembl = try(useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "dec2016.archive.ensembl.org"), silent = T)
         if (class(ensembl) == "try-error") {
-            ensembl = useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "grch38.ensembl.org")
+            ensembl = try(useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "grch38.ensembl.org"))
         }
-    } else {
+    } 
+    if (is.null(ensembl) || class(ensembl) == "try-error") {
         warning(paste("GRCh", GRCh, "not supported. Only supporting GRCh=36, GRCh=37 or GRCh=38. Abborting."))
         return()
     }
